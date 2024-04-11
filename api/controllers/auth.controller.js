3aa74ce1ae965557976b09f2db2e045d6feb72f2
KhaledSaeed18 <khaled18saeed@gmail.com>
2024-04-11 16:58:36 +0300

    create signin API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-import { error } from "console";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -26,7 +26,45 @@ export const signup = async (req, res, next) => {
     catch (error) {
         next(error);
     }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required !'));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found !'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, 'Invalid password !'));
+        }
+
+        const token = jwt.sign(
+            {
+                id: validUser._id // Add the user id to the token payload
+            },
+            process.env.JWT_SECRET, // Add the secret key to the token
+            {
+                expiresIn: '2h' // Set the token expiration time
+            }
+        );
+
+        const { password: pass, ...rest } = validUser._doc; // Remove the password from the user object
+
+        res.status(200).cookie('token', token, {
+            httpOnly: true,
+        }).json(rest);
 
 
+    }
+    catch (error) {
+        next(error);
+    }
 
 };
