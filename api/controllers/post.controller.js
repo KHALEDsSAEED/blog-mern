a88a3d79d0a6aace974349f12c3edbedca7a773c
KhaledSaeed18 <khaled18saeed@gmail.com>
2024-06-10 21:03:19 +0300

    Create GET posts API route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -25,3 +25,42 @@ export const create = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getposts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0; // Default value is 0 if not provided 
+        const limit = parseInt(req.query.limit) || 9; // Default value is 9 if not provided
+        const sortDiretion = req.query.order === 'asc' ? 1 : -1; // if 1 then ascending, if -1 then descending
+        const posts = await Post.find({
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { category: req.query.category }),
+            ...(req.query.slug && { slug: req.query.slug }),
+            ...(req.query.postId && { _id: req.query.postId }),
+            ...(req.query.searchTerm && {
+                $or: [ // or is used to search for a term in multiple fields
+                    { title: { $regex: req.query.searchTerm, $options: 'i' } }, // regex is used to search for a term in a string, i is for case-insensitive
+                    { content: { $regex: req.query.searchTerm, $options: 'i' } },
+                ],
+            }),
+        }).sort({ createdAt: sortDiretion }).skip(startIndex).limit(limit);
+
+        const totalPosts = await Post.countDocuments();
+
+        const timeNow = new Date();
+
+        const oneMonthAgo = new Date(
+            timeNow.getFullYear(),
+            timeNow.getMonth() - 1,
+            timeNow.getDate(),
+        );
+
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: { $gte: oneMonthAgo }, // gte is used to get the posts created after the date
+        });
+
+        res.status(200).json({ posts, totalPosts, lastMonthPosts });
+
+    } catch (error) {
+        next(error);
+    }
+};
