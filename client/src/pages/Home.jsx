fa1d031a5126ec276336106cb33df255592495c5
KhaledSaeed18 <khaled18saeed@gmail.com>
2024-06-12 18:31:43 +0300

    Add new image and a link to the sign in page to the hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Lottie from 'lottie-react';
 import animationData from '../assets/Animation - 1718203405122.json';
 import FlipWords from '../components/FlipWords';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
+    const { currentUser } = useSelector((state) => state.user);
     const words = ["Tips", "Tricks", "Guides", "News", "Tutorials"];
     return (
         <div className=''>
-            <div className="flex flex-col h-[60vh] md:flex-row mx-20">
+            <div className="flex flex-col sm:h-[85vh] md:h-[60vh]  md:flex-row mx-20">
                 <div className="flex-1 flex items-center justify-center py-6">
                     <div className="md:text-2xl font-normal text-neutral-600 dark:text-white">
                         Welcome to our blog, where we share,
@@ -16,12 +19,13 @@ export default function Home() {
                         in web development.
                         <br />
                         <br />
-                        Join our community and stay updated with the best practices and innovative solutions to enhance your web projects.
+                        {!currentUser ?
+                            <Link to='/sign-in'><span className=' text-[#DA4AA2] font-extrabold hover:text-3xl'>Join our community</span></Link> : <span>Join our community</span>}   and stay updated with the best practices and innovative solutions to enhance your web projects.
                     </div>
                 </div>
 
-                <div className="flex-1">
-                    <Lottie animationData={animationData} className=' h-full p-6' />
+                <div className="flex-1 flex items-center justify-center py-6">
+                    <Lottie animationData={animationData} className=' h-96 md:h-[100%] ' />
                 </div>
             </div>
         </div>
