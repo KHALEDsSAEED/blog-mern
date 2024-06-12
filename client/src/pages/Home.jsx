406c5e25ed87ba4a759e17e9bd82daed4d51ae4c
KhaledSaeed18 <khaled18saeed@gmail.com>
2024-06-13 01:48:38 +0300

    Edit UI in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,8 +13,8 @@ export default function Home() {
                 <div className="flex-1 flex items-center justify-center py-6">
                     <div className="md:text-2xl font-normal text-neutral-600 dark:text-white">
                         Welcome to our blog, where we share,
-                        <span className='bg-gradient-to-r mx-2 md:text-4xl rounded-lg text-white from-indigo-500 via-purple-500 to-pink-500'>
-                            <FlipWords words={words} duration={2000} />
+                        <span className='bg-gradient-to-r mx-2 md:text-4xl rounded-lg from-indigo-500 via-purple-500 to-pink-500'>
+                            <FlipWords words={words} duration={2000}/>
                         </span>
                         in web development.
                         <br />
@@ -25,7 +25,7 @@ export default function Home() {
                 </div>
 
                 <div className="flex-1 flex items-center justify-center py-6">
-                    <Lottie animationData={animationData} className=' h-96 md:h-[100%] ' />
+                    <Lottie animationData={animationData} className=' h-96 md:h-[100%]' />
                 </div>
             </div>
         </div>
