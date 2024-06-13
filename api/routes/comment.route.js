276e1ad0e7d8b3fdd4de9b1c24fe56d6081f34e7
KhaledSaeed18 <khaled18saeed@gmail.com>
2024-06-14 01:48:51 +0300

    Show comments of a post

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createcomment } from '../controllers/comment.controller.js';
+import { createcomment, getPostComments } from '../controllers/comment.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createcomment);
+router.get('/getpostscomment/:postId', getPostComments);
 
 export default router;
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser, deleteUser, signout, getusers } from '../controllers/user.controller.js';
+import { test, updateUser, deleteUser, signout, getusers, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -12,5 +12,6 @@ router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getusers);
+router.get('/:userId', getUser);
 
 export default router;
